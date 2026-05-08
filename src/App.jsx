7ed27a52b546308063757c9dea0497cba8f09faf
mahmoudsalah67 <mahmoudsalah@example.com>
2026-05-08 15:38:04 +0300

    update home and jobs components

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
       
        { path: "", element: <Home /> },
        { path: "about", element: <About /> },
-      { path: "jobsس", element: <Jobs /> },
+      { path: "jobs", element: <Jobs /> },
       { path: "contact", element: <Contact /> },
       { path: "login", element: <Login /> },
       {path:'joinnow',element:<JoinNow/>},
